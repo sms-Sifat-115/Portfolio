0bc705883130cbc5beeba000509f92b9a2bc60d5
S M Sanbim Sifat <79338515+sms-Sifat-115@users.noreply.github.com>
2024-09-19 16:46:43 +0600

    Tenth Draft: Glow Effect Added

--- app.js
+++ app.js
@@ -45,3 +45,17 @@ function showInitialDots() {
 }
 
 showInitialDots();
+
+
+/* -- Glow effect -- */
+
+const blob = document.getElementById("blob");
+
+window.onpointermove = event => { 
+  const { clientX, clientY } = event;
+  
+  blob.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+  }, { duration: 3000, fill: "forwards" });
+}
